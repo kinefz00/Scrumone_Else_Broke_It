196e2e101cda19640d2708950a82a97c1f433b1a
Kim Neuhäusler <kim.neuhaeusler@studmail.hs-aalen.de>
2022-10-28 23:04:13 +0200

    init Impressum first stage

---
+++ src/main/java/Scrumone_Else_Broke_It/WebShop/entity/Impressum.java
@@ -0,0 +1,19 @@
+package Scrumone_Else_Broke_It.WebShop.entity;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Id;
+@Getter
+@Setter
+@Data
+public class Impressum {
+    @Id
+    @Column
+    private int id;
+    @Column
+    private String impressum;
+}
