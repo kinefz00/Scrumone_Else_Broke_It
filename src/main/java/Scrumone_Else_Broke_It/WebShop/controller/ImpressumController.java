88e6342406084b6c4bd4ac37fcb11188653b0570
FmLFable <67640045+FmLFable@users.noreply.github.com>
2022-11-02 21:27:57 +0100

    add crossOrigin request

--- src/main/java/Scrumone_Else_Broke_It/WebShop/controller/ImpressumController.java
+++ src/main/java/Scrumone_Else_Broke_It/WebShop/controller/ImpressumController.java
@@ -7,16 +7,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+
 public class ImpressumController {
 
     @Autowired
     ImpressumService impressumService;
-
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/impressum/{id}")
     private Impressum getImpressumId(@PathVariable("id") int id) {
         return impressumService.getImpressumById(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/impressum")
     public Impressum saveImpressum(@RequestBody Impressum impressum){
         impressumService.saveOrUpdateImpressum(impressum);
--- src/main/java/Scrumone_Else_Broke_It/WebShop/controller/ProductController.java
+++ src/main/java/Scrumone_Else_Broke_It/WebShop/controller/ProductController.java
@@ -13,26 +13,32 @@ public class ProductController {
     @Autowired
     ProductService productService;
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/product")
     private List<Product> getAllProduct(){
         return productService.getAllProduct();
     }
+
+    @CrossOrigin(origins = "http://localhost:4200")
     @GetMapping("/product/{id}")
     private Product getProductId(@PathVariable("id") int id) {
         return productService.getProductById(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PostMapping("/product")
     public Product saveProduct(@RequestBody Product product){
         productService.saveOrUpdate(product);
         return product;
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @DeleteMapping("/product/{id}")
     public void deleteProduct(@PathVariable int id){
         productService.deleteProduct(id);
     }
 
+    @CrossOrigin(origins = "http://localhost:4200")
     @PutMapping("/product/{id}")
     public void editProduct(@PathVariable int id, @RequestBody Product product){
         productService.edit(id, product);
