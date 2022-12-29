5ffb971eebfe3e2420fdffbadd9f55daebde21db
Kim Neuhäusler <kim.neuhaeusler@studmail.hs-aalen.de>
2022-12-29 13:40:35 +0100

    update controller + service

--- src/main/java/Scrumone_Else_Broke_It/WebShop/controller/DeliveryAddressController.java
+++ src/main/java/Scrumone_Else_Broke_It/WebShop/controller/DeliveryAddressController.java
@@ -1,10 +1,7 @@
 package Scrumone_Else_Broke_It.WebShop.controller;
-import Scrumone_Else_Broke_It.WebShop.entity.Product;
-import Scrumone_Else_Broke_It.WebShop.entity.TechnicalDetails;
+import Scrumone_Else_Broke_It.WebShop.entity.DeliveryAddress;
 import Scrumone_Else_Broke_It.WebShop.repository.DeliveryAddressRepository;
 import Scrumone_Else_Broke_It.WebShop.service.DeliveryAddressService;
-import Scrumone_Else_Broke_It.WebShop.service.ProductService;
-import Scrumone_Else_Broke_It.WebShop.service.TechnicalDetailsService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -20,5 +17,40 @@ public class DeliveryAddressController {
     @Autowired
     DeliveryAddressRepository deliveryAddressRepository;
 
+    @GetMapping("/deliveryAddress")
+    private List<DeliveryAddress> getAllDeliveryAddress() {
+        return deliveryAddressService.getDeliveryAddressList();
+    }
 
+    @GetMapping("/deliveryAddress/{id}")
+    private DeliveryAddress getDeliveryAddressId(@PathVariable("id") int id) {
+        return deliveryAddressService.getDeliveryAddressById(id);
+    }
+
+
+    @PostMapping("/deliveryAddress")
+    public void saveDeliveryAddress(@RequestBody DeliveryAddress deliveryAddress) {
+        deliveryAddressService.saveDeliveryAddress(deliveryAddress);
+
+    }
+
+
+    @DeleteMapping("/deliveryAddress/{id}")
+    public void deleteDeliveryAddress(@PathVariable int id) {
+        deliveryAddressService.deleteDeliveryAddress(id);
+    }
+
+
+    @PutMapping("/deliveryAddress/{id}")
+    public void editDeliveryAddress(@PathVariable int id, @RequestBody DeliveryAddress deliveryAddress) {
+        deliveryAddressService.edit(id, deliveryAddress);
+    }
+
+    @PutMapping("/{deliveryAddressId}/user/{username}")
+    public void assignDeliveryAddressToUser(
+            @PathVariable int deliveryAddressId,
+            @PathVariable String username
+    ) {
+        this.deliveryAddressService.assignDeliveryAddressToUser(deliveryAddressId, username);
+    }
 }
--- src/main/java/Scrumone_Else_Broke_It/WebShop/service/DeliveryAddressService.java
+++ src/main/java/Scrumone_Else_Broke_It/WebShop/service/DeliveryAddressService.java
@@ -1,10 +1,68 @@
 package Scrumone_Else_Broke_It.WebShop.service;
 
 
+import Scrumone_Else_Broke_It.WebShop.controller.TechnicalDetailsController;
+import Scrumone_Else_Broke_It.WebShop.entity.DeliveryAddress;
+import Scrumone_Else_Broke_It.WebShop.entity.UserEntity;
+import Scrumone_Else_Broke_It.WebShop.repository.DeliveryAddressRepository;
+import Scrumone_Else_Broke_It.WebShop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public class DeliveryAddressService {
 
+    public static Logger logger = Logger.getLogger(String.valueOf(TechnicalDetailsController.class));
+    @Autowired
+    public DeliveryAddressRepository deliveryAddressRepository;
+    @Autowired
+    public UserRepository userRepository;
 
+
+    public void saveDeliveryAddress(DeliveryAddress deliveryAddress){
+        logger.info("Angelegt");
+        deliveryAddressRepository.save(deliveryAddress);
+    }
+
+
+    public List<DeliveryAddress> getDeliveryAddressList() {
+        List<DeliveryAddress> list = new ArrayList<>();
+        Iterator<DeliveryAddress> it = deliveryAddressRepository.findAll().iterator();
+        while (it.hasNext()){
+            list.add(it.next());
+        }
+        return list;
+    }
+
+    public DeliveryAddress getDeliveryAddressById(int id) {
+        logger.info("Get \"TechnicalDetails\" with the id: " + id);
+        return deliveryAddressRepository.findById(id).get();
+    }
+
+    public void deleteDeliveryAddress(int id) {
+        logger.info("Delete \"TechnicalDetails\" with id: " + id);
+        deliveryAddressRepository.deleteById(id);
+    }
+
+    public void edit(int id, DeliveryAddress deliveryAddress) {
+        deliveryAddress.setId(id);
+        deliveryAddressRepository.save(deliveryAddress);
+        logger.info("Edit \"TechnicalDetails\" with id: " + id);
+    }
+    public DeliveryAddress assignDeliveryAddressToUser(
+            @PathVariable int deliveryAddressId,
+            @PathVariable String username
+    ){
+        DeliveryAddress deliveryAddress = deliveryAddressRepository.findById(deliveryAddressId).get();
+        UserEntity user = userRepository.findById(username).get();
+        deliveryAddress.assignUser(user);
+        return deliveryAddressRepository.save(deliveryAddress);
+
+    }
 }
